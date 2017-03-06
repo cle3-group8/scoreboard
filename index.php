8cd95ac43f5ce2b863e0b83a93a9d960d151c5bc
Goef <geoffrey0807@hotmail.com>
2017-03-06 10:05:49 +0100

    ja

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <title>Naam</title>
 
     <link rel="stylesheet" href="/css/normalize.css">
-
+    <link rel="stylesheet" href="css/Style.css">
+    <script src="js/jquery.js"></script>
     <script src="https://cdn-orig.socket.io/socket.io-1.4.5.js"></script>
-
 </head>
 <body>
+
+
+<ul id="playerlist">
+
+
+</ul>
+
+
+
+
+
+
+
+
+<script src="js/main.js"></script>
 
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 var serverAddress = 'http://127.0.0.1:3000';
-var socket = io(serverAddress);
+var socket = io("http://game.maashaven.win:3000");
+var playernames = [];
+
+
+socket.on("newplayer", function (data) {
+    console.log(data);
+    playernames.push(data.name);
+    var element = $("<li>" + data.name +  + data.score+ "</li>");
+
+    element.css('background-color' , data.color);
+    $("#playerlist").prepend(element);
+
+});
+
+socket.emit("scorebord", "hey");
+
